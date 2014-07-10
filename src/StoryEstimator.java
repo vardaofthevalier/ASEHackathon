0f792b5533b4dc079599f017591b480f7c8388c1
abigail <abigail.hahn@ca.com>
2014-07-10 19:04:00 -0400

    Added some structured comments

--- src/StoryEstimator.java
+++ src/StoryEstimator.java
@@ -1,16 +1,43 @@
 
 public class StoryEstimator {
+	
+	/* Story to csv function
+	 * 
+	 */
+	
+	/* The story parsing function
+	 * 	- Should parse the story, removing filler words.
+	 * 	- Determine relevant content
+	 * 	- Create and return a map of words to frequencies
+	 */
+	
+	/* AARF generator function
+	 *  
+	 */
+	
+	/* Training set creator function
+	 * 
+	 */
+	
+	/* Classification function
+	 * 
+	 */
+	
+	/* Comparison and visualization function
+	 * 
+	 */
 
 	public static void main(String[] args) {
 		
 		/* Stuff to do 
 		 * 	1) Get story data, remove quotes and commas, store in a csv file
-		 * 	2) Parse story data to determine relevant content, and remove filler words
+		 * 	2) For each story: 
+		 * 		a) parse the data to determine relevant content, and remove filler words 
+		 * 		b) create an ARFF file for Weka processing
 		 * 	3) Create the training set using about half of the data
 		 * 	4) Classify remaining data to determine estimates
 		 * 	5) Compare estimates with true completion times
 		 * */
-
 	}
 
 }
