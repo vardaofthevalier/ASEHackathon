b4be960d71de9255b0290879934a5ad63412d8f5
abigail <abigail.hahn@ca.com>
2014-07-10 17:55:14 -0400

    Initial commmit

---
+++ src/StoryEstimator.java
@@ -0,0 +1,16 @@
+
+public class StoryEstimator {
+
+	public static void main(String[] args) {
+		
+		/* Stuff to do 
+		 * 	1) Get story data, remove quotes and commas, store in a csv file
+		 * 	2) Parse story data to determine relevant content, and remove filler words
+		 * 	3) Create the training set using about half of the data
+		 * 	4) Classify remaining data to determine estimates
+		 * 	5) Compare estimates with true completion times
+		 * */
+
+	}
+
+}
